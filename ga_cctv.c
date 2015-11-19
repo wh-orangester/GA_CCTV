4e2a0f54e418f8370b0fc872abbd04d77e506c1e
wh_orangester <w.h.orangester@gmail.com>
2015-11-19 20:16:20 +0700

    Function for Fitness Evaluation Added
    For Problem #1: Fitness is proportional to secureness.

--- ga_cctv.c
+++ ga_cctv.c
@@ -101,9 +101,38 @@ gene parentSelection(gene * population) {
 gene crossingOver(gene parent1, gene parent2) {
 
 }
-void evaluateFitness(gene * individual) {
-    // an algorithm to evaluate individual's fitness
-    // applying penalty
+void evaluateAverageSecureness(gene * individual) {
+    double secureness[MAX_CCTV][MAX_ARTIFACT], a_secureness;
+    double min_vrange, max_vrange, deg;
+    int i, j;
+    int visible;
+
+    for(i = 0; i < c_count; i++) {
+        if((*individual).type[i] == 0) {
+            for(j = 0; j < a_count; j++) secureness[i][j] = 0;
+        }
+        else {
+            min_vrange = (*individual).dir[i] - (cva[(*individual).type[i]] / 2);
+            max_vrange = (*individual).dir[i] + (cva[(*individual).type[i]] / 2);
+            max_vrange -= (max_vrange > 180) ? 360 : 0;
+            for(j = 0; j < a_count; j++) {
+                if(ca_dis[i][j] == -1) secureness[i][j] = 0;
+                else {
+                    deg = ca_deg[i][j];
+                    visible = (min_vrange <= max_vrange) ? (deg > min_vrange && deg < max_vrange) : (deg < min_vrange || deg > min_vrange);
+                    if(!visible) secureness[i][j] = 0;
+                    else secureness[i][j] = 1 / (1 + (ca_dis[i][j] / cr[(*individual).type[i]]));
+                }
+            }
+        }
+    }
+
+    (*individual).fitness = 0;
+    for(j = 0; j < a_count; j++) {
+        a_secureness = 1;
+        for(i = 0; i < c_count; i++) a_secureness = a_secureness * (1 - secureness[i][j]);
+        (*individual).fitness += (1 / a_count) * (1 - a_secureness);
+    }
 }
 void sortPopulation(gene * population) {
     // sort individuals in the population according to their fitness
